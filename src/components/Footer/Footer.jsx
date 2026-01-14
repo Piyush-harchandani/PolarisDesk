d6a3d72700bf42d951fce1e9a613540485f1e508
Piyush-harchandani <157939734+Piyush-harchandani@users.noreply.github.com>
2026-01-14 05:31:48 +0530

    Update README.md

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Footer.css'
+
+export const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>Copyright @ 2025, Crypto app - All Right Reserved.</p>
+    </div>
+  )
+}
